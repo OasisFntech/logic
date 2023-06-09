fe526487cc54f3193dd4fa29ee84f2f639898f62
Cyan <cyanbiubiu@proton.me>
2023-06-09 08:06:02 +0400

    1

--- main.js
+++
@@ -1,4 +0,0 @@
-export { default as fetch } from './src/fetch'
-export { default as store } from './src/store'
-export { default as utils } from './src/utils'
-export { default as hooks } from './src/hooks'
