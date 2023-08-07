883036ea6921e4f7e9675d4b101de76372759069
Cyan <132916562+Cyan-963852@users.noreply.github.com>
2023-08-07 11:46:32 +0400

    0.0.68 usePublicKeyStore

--- store/publicKey.js
+++ store/publicKey.js
@@ -3,12 +3,13 @@ import { defineStore } from 'pinia'
 import { useRequest, COMMON_API_PATH } from '../fetch'
 
 export const usePublicKeyStore = defineStore('publicKey', () => {
-    const { response } = useRequest({
+    const { response, onRefresh } = useRequest({
         url: COMMON_API_PATH.PUBLIC_KEY,
         initialValues: ''
     })
 
     return {
-        publicKey: response
+        publicKey: response,
+        onUpdatePublicKey: onRefresh
     }
 })
