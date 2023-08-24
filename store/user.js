03993bdd16a7fdf50f1dfdaabec759922d3aa344
Cyan <132916562+Cyan-963852@users.noreply.github.com>
2023-08-24 13:49:50 +0400

    0.0.96 useUserInfoStore

--- store/user.js
+++ store/user.js
@@ -5,66 +5,68 @@ import _ from 'lodash'
 import { COMMON_API_PATH, useRequest } from '../fetch'
 import { utils_assign_object } from '../utils'
 
+const initialValues = {
+    // 账号
+    account: '',
+    // id
+    memberId: - 1,
+    // 手机号
+    phone: '',
+    // token
+    token: '',
+    // token 过期时间
+    tokenExpiration: 0,
+
+    // 昵称
+    nickname: '',
+    // 头像
+    avatarUrl: '',
+    // vip 等级
+    level: '',
+    commissionPercent: null,
+    // 总资产
+    totalAssets: '--',
+    // 钱包余额
+    amount: '--',
+    // 提现冻结
+    withdrawFreeze: '--',
+    // 合约净资产
+    contractPrincipal: '--',
+    // 总操盘资金
+    totalTradersMoney: '--',
+    // 未使用
+    surplusAmount: '--',
+    // 已使用
+    usedAmount: '--',
+    expiryAmount: null,
+    count: null,
+    subCount: null,
+    commission: null,
+    recharge: null,
+    rechargeSum: null,
+    status: null,
+    inviteLink: null,
+    backMoneyFreeze: '--',
+    realBalance: null,
+    isAgent: true,
+    netAssets: '--',
+
+    email: null,
+    // 邀请码
+    inviteCode: '',
+    // 真实姓名
+    realName: null,
+    customerService: null,
+    pid: '',
+    customerServiceId: null,
+    memberLevelId: '--',
+    handlingFeePercent: '--',
+    interestFeePercent: '--',
+    withdrawAmount: '--'
+}
+
 export const useUserInfoStore = defineStore('userInfo', () => {
-    const userInfo = ref({
-        // 账号
-        account: '',
-        // id
-        memberId: - 1,
-        // 手机号
-        phone: '',
-        // token
-        token: '',
-        // token 过期时间
-        tokenExpiration: 0,
-
-        // 昵称
-        nickname: '',
-        // 头像
-        avatarUrl: '',
-        // vip 等级
-        level: '',
-        commissionPercent: null,
-        // 总资产
-        totalAssets: '--',
-        // 钱包余额
-        amount: '--',
-        // 提现冻结
-        withdrawFreeze: '--',
-        // 合约净资产
-        contractPrincipal: '--',
-        // 总操盘资金
-        totalTradersMoney: '--',
-        // 未使用
-        surplusAmount: '--',
-        // 已使用
-        usedAmount: '--',
-        expiryAmount: null,
-        count: null,
-        subCount: null,
-        commission: null,
-        recharge: null,
-        rechargeSum: null,
-        status: null,
-        inviteLink: null,
-        backMoneyFreeze: '--',
-        realBalance: null,
-        isAgent: true,
-        netAssets: '--',
-
-        email: null,
-        // 邀请码
-        inviteCode: '',
-        // 真实姓名
-        realName: null,
-        customerService: null,
-        pid: '',
-        customerServiceId: null,
-        memberLevelId: '--',
-        handlingFeePercent: '--',
-        interestFeePercent: '--',
-        withdrawAmount: '--'
-    })
+    const userInfo = ref(...initialValues)
 
     const balance_visible = ref(false),
         balance = computed(() => balance_visible.value ? userInfo.value.totalAssets : '******')
@@ -111,7 +113,12 @@ export const useUserInfoStore = defineStore('userInfo', () => {
         onRefreshUserInfo
     }
 }, {
-    persist: true
+    persist: true,
+    actions: {
+        reset() {
+            this.userInfo = { ...initialValues }
+        }
+    }
 })
 
 export const useMessageStore = defineStore('message', () => {
