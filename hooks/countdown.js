8db1dc9a25f3ce6169de3c4a06ba6b967ce08ddd
Cyan <132916562+Cyan-963852@users.noreply.github.com>
2023-08-29 12:17:32 +0400

    0.0.106 useCountdown

--- hooks/countdown.js
+++ hooks/countdown.js
@@ -1,34 +1,31 @@
-import { onMounted } from 'vue'
-import { useIntervalFn, useLocalStorage } from '@vueuse/core'
+import { useLocalStorage } from '@vueuse/core'
+
+let countdown_interval = null
 
 export const useCountdown = (name = 'noname') => {
     const key = `${name}-countdown`,
         countdown = useLocalStorage(key, 0)
 
     // 倒计时函数
-    const { pause, resume } = useIntervalFn(() => {
-        if (countdown.value > 0) {
-            countdown.value --
-        } else {
-            localStorage.removeItem(key)
-            pause()
-        }
-    }, 1000)
+    const onStart = () => {
+        countdown_interval = setInterval(() => {
+            if (countdown.value > 0) {
+                countdown.value --
+            } else {
+                localStorage.removeItem(key)
+                clearInterval(countdown_interval)
+                countdown_interval = null
+            }
+        }, 1000)
+    }
 
     // 开始倒计时
     const onCountdown = (time = 60) => {
         countdown.value = time
-        resume()
+        onStart()
     }
 
-    // 页面加载时有未完成倒计时，继续倒计时
-    onMounted(() => {
-        if (countdown.value > 0) resume()
-    })
-
     return {
-        pause,
-        resume,
         countdown,
         onCountdown
     }
