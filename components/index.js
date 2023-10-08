b17218306146cc34ca61262d08390c76204d479f
Cyan <132916562+Cyan-963852@users.noreply.github.com>
2023-10-08 06:43:23 +0400

    0.0.121 delete components

--- components/index.js
+++
@@ -1,9 +0,0 @@
-import CustomForm from './CustomForm/CustomForm.vue'
-import ControllerRender from './CustomForm/ControllerRender'
-import Picker from './Picker/index.vue'
-
-export {
-    CustomForm,
-    ControllerRender,
-    Picker
-}
