aec10c9aa61a566b28199977a7ce50b68d5a7414
JioJio <@>
2024-11-30 15:31:16 +0400

    更新依赖、新增初始化配置接口调试

--- hooks/index.js
+++ hooks/index.js
@@ -1,6 +1,7 @@
 import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useTitle } from '@vueuse/core'
+import VConsole from 'vconsole'
 import _ from 'lodash'
 
 import { api_fetch, COMMON_API_PATH } from '../fetch'
@@ -17,19 +18,22 @@ export * from './news'
 export const useFormDisabled = (formState, omits) => computed(() => !_.every(_.omit(formState, omits), Boolean))
 
 export const useSiteConfig = async(callback) => {
-    const res = await api_fetch({
-        url: COMMON_API_PATH.SITE_CONFIG
-    })
-
     try {
+        const res = await api_fetch({
+            url: COMMON_API_PATH.SITE_CONFIG
+        })
+        useTitle(res.siteName)
+        utils_favicon(res.titleAddress)
+
         await callback(res.siteSwitch)
 
         const { siteConfig } = storeToRefs(useSiteConfigStore())
 
         siteConfig.value = utils_assign_object(siteConfig.value, res)
-    } finally {
-        useTitle(res.siteName)
-        utils_favicon(res.titleAddress)
+    } catch (e) {
+        new VConsole()
+        console.error(e)
+        throw new Error(e)
     }
 }
 
